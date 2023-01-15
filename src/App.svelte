9bdcc1461d3bb24c2bb2ce4a8d9254afedf6f02b
AarushJ19 <aarushjain135@gmail.com>
2023-01-15 23:01:57 +0530

    UI changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,84 +1,32 @@
 <script>
-import Header from './header.svelte'
-
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
+import Header from "./header.svelte";
 </script>
-  
+
 <main class="fullscreen">
-  <h1 style="font-family:Centaur"><h1 style="font-size:50px">CHILLFLIX</h1></h1>
-  <Header/>
-  <video autoplay muted loop id="myVideo">
-    <source src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4">
-  </video>
-  <div class="content">
-  
-    <!-- Use a button to pause/play the video with JavaScript -->
-    <!-- <button id="myBtn" on:click={myFunction}>Pause</button> -->
-  </div>
+<div class="menuribbon">
+<img src="https://pbs.twimg.com/media/EUcW85oXsAAnIBP.jpg" class="logo" alt="logo">
+<Header/>
+</div>
+
 </main>
+
 
 <style>
 
-#myVideo {
-  position: relative;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 400px;
-  /* overflow:visible; */
-}
-
 .fullscreen{
-  top:0;
-  right:0;
-  left:0;
-  bottom:0;
-  overflow:auto;
-  /* background-color: indigo; */
+background-color: black;
 }
 
-h1{
-  overflow:auto;
-  padding:20px;
-  text-align: center;
-  color:yellow;
-  width:auto;
-  height:20%;
-  margin:0%;
-  border: 2px solid red;
-  background-color: black;
-}    
-:root{
-      background-color: #181818;
-    }
-
-    /* Add some content at the bottom of the video/page */
-.content {
-  position: fixed;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  color: #f1f1f1;
-  width: 100%;
-  padding: 20px;
+.logo{
+height:120px;
+width:170px;  
 }
 
-/* Style the button used to pause/play the video */
-#myBtn {
-  width: 200px;
-  font-size: 18px;
-  padding: 10px;
-  border: none;
-  background: #000;
-  color: #fff;
-  cursor: pointer;
+.menuribbon{
+  display: flex;
+  flex-direction: row;
+  width:100%;
 }
 
-#myBtn:hover {
-  background: #ddd;
-  color: black;
-}
+
 </style>
--- src/header.svelte
+++ src/header.svelte
@@ -13,6 +13,7 @@
         </select>
         <!-- <th>Movies</th> -->
         <button class="menubutton">Mylist</button>
+        <button>Sign In</button>
     </div>
 
 </main>
@@ -22,50 +23,60 @@
   display: flex;
   flex-direction: row;
   width:100%;
-  background-color:rgba(247, 55, 55, 0.988) ;
+  /* background-color: black ; */
 }
 
 .dropdownbutton{
-  background-color:rgba(247, 55, 55, 0.988);
+
   font-size: 30px;
-  border: 80px red;
+  border: 80px red; 
   text-align: center;
   width:220px;
+  background-color: black;
+  color:white;
+  font-family: Bebas Neue;
   /* text-decoration: none;  */
   /* display: inline-block; */
+  height: 120px;
   padding: 10px 25px;
   transition: width 3s, height 3s;
-  opacity: 95%;
-  font-weight: bold;
+  /* opacity: 95%; */
+  /* font-weight: bold; */
 }
 .dropdownbutton:hover{
   text-align: center;
   align-items: center;
   opacity:100%;
   width:300px;
-  height:100px;
+  height:120px;
   background-color: yellow;
+  font-family: Bebas Neue;
+  color:red;
 }
 .menubutton{
-  background-color:rgba(247, 55, 55, 0.988);
+  font-family: Bebas Neue;
+  background-color: transparent;  
+  color:white;
   font-size: 25px;
-  border: 20px black;
+  border: 80px red; 
   text-align: center;
   width:150px;
+  height: 120px;
   /* text-decoration: none;  */
   /* display: inline-block; */
   padding: 10px 25px;
   transition: width 3s, height 3s;
-  opacity: 95%;
-  font-weight: bold;
+  /* opacity: 95%; */
+  /* font-weight: bold; */
 }
 .menubutton:hover{
+  color:red;
   text-align: center;
   align-items: center;
-  opacity:100%;
+  /* opacity:100%; */
   width:300px;
-  height:100px;
+  font-family: Bebas Neue;
+  height:120px;
   background-color: yellow;
 }
-
 </style>
